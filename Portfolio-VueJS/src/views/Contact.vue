75739bdc5b295c0cb94867d83941c7dbed6045c5
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-05-27 17:39:55 -0300

    Iniciando pagina de contato

--- Portfolio-VueJS/src/views/Contact.vue
+++ Portfolio-VueJS/src/views/Contact.vue
@@ -2,5 +2,37 @@
 </script>
 
 <template>
-    <h1>Pagina de contato</h1>
+    <section class="min-h-screen flex flex-col items-center justify-center text-center">
+        <div class="p-8" >
+            <h1 class="text-6xl font-semibold text-white mt-12 mb-32">Entre em contato comigo</h1>
+            <p class="text-white text-lg mb-12">Caso tenha gostado do meu perfil e do que sou capaz,<br>sinta-se vontade para entrar em contaro por algum destes canais!</p>
+        </div>
+        <div class="rounded-xl  flex flex-row w-full justify-center gap-5 p-5" >
+            <div class=" border border-white/30 rounded-2xl p-8 w-64 hover:scale-105 hover:shadow-xl transition transform duration-300">
+                <h1>Whatsapp</h1>
+                <i class="mb-5 text-green-400 text-7xl fa-brands fa-square-whatsapp"></i>
+                <p class="text-gray-300">Mande uma mensagem!</p>
+            </div>
+            <div class=" border border-white/30 rounded-2xl p-8 w-64 hover:scale-105 hover:shadow-xl transition transform duration-300">
+                <h1>Github</h1>
+                <i class="mb-5 text-neutral-200 text-7xl fa-brands fa-square-github"></i>
+                <p class="text-gray-300">Veja meus projetos.</p>
+            </div>
+            <div class=" border border-white/30 rounded-2xl p-8 w-64 hover:scale-105 hover:shadow-xl transition transform duration-300">
+                <h1>Linkedin</h1>
+                <i class="mb-5 text-blue-500 text-7xl fa-brands fa-linkedin"></i>
+                <p class="text-gray-300">Conecte-se comigo!</p>
+            </div>
+            <div class=" border border-white/30 rounded-2xl p-8 w-64 hover:scale-105 hover:shadow-xl transition transform duration-300">
+                <h1>Email</h1>
+                <i class="mb-5 text-red-700 text-7xl fa-solid fa-square-envelope"></i>
+                <p class="text-gray-300">Envie um email.</p>
+            </div>
+        </div>  
+    
+    </section>
+    
 </template>
+
+<style scoped>
+</style>
